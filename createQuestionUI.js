cfc0f224a1c6bc3486e40b8acd8efbc5a05d43e5
Trần Đắc Dương <71543016+TranDacDuong@users.noreply.github.com>
2026-03-09 22:37:20 +0700

    Update createQuestionUI.js

--- createQuestionUI.js
+++ createQuestionUI.js
@@ -345,3 +345,25 @@ INIT
 
 loadDifficulty()
 renderAnswerUI()
+
+/* =========================
+MODAL
+========================= */
+
+const modal = document.getElementById("modal")
+
+function openModal(){
+
+if(!modal) return
+
+modal.style.display = "flex"
+
+}
+
+function closeModal(){
+
+if(!modal) return
+
+modal.style.display = "none"
+
+}
