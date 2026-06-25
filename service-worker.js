3ebacf8c3813ea451751a51f489bb067a73b9338
Tran Dac Duong <trandacduong224@gmail.com>
2026-06-25 13:56:58 +0700

    fix: auto logout when profile is not found in public.users

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "mindup-pwa-v22";
+const CACHE_VERSION = "mindup-pwa-v23";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const PUSH_RECEIPT_CACHE = `${CACHE_VERSION}-push-receipts`;
