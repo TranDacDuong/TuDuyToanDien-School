f068a43a7758c94cd098c6da07fe68ed0e5a85af
Tran Dac Duong <trandacduong224@gmail.com>
2026-09-24 10:17:08 +0700

    Keep Zalo gateway running on battery

--- services/zalo-bot/install-autostart.ps1
+++ services/zalo-bot/install-autostart.ps1
@@ -10,7 +10,7 @@ $userId = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $action = New-ScheduledTaskAction -Execute $node -Argument 'server.js' -WorkingDirectory $botDir
 $trigger = New-ScheduledTaskTrigger -AtLogOn -User $userId
 $principal = New-ScheduledTaskPrincipal -UserId $userId -LogonType Interactive -RunLevel Limited
-$settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1) -StartWhenAvailable
+$settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1) -StartWhenAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Principal $principal -Settings $settings -Description 'Start the local MindUp Zalo gateway when this user signs in' -Force -ErrorAction Stop | Out-Null
 if (-not (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue)) {
   throw 'Scheduled task registration could not be verified.'
